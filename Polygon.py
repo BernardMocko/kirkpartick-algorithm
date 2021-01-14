206e0869d0509b9a44bfb5abfd9d807ee68ffa4a
BernardMocko <76700038+BernardMocko@users.noreply.github.com>
2021-01-14 03:01:24 +0100

    Update Polygon.py

--- Polygon.py
+++ Polygon.py
@@ -226,7 +226,6 @@ class Polygon:
                                (vertex.point.y-sweepline[i][1][1].point.y)+sweepline[i][1][1].point.x)
                             curr_dis-=vertex.point.x
                             curr_dis*=-1
-                            type(curr_dis)
                             if curr_dis>0 and curr_dis<distance:
                                 distance=curr_dis
                                 indeks=i
